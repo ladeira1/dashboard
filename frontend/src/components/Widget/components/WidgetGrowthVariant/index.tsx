6d8d7f42c7d6bbb44b1e9f2f68620ded5659dd26
Joao Ladeira <joaoladeirag@gmail.com>
2023-11-29 21:55:59 -0300

    refactor: WidgetGrowthVariant

--- frontend/src/components/Widget/components/WidgetGrowthVariant/index.tsx
+++ frontend/src/components/Widget/components/WidgetGrowthVariant/index.tsx
@@ -1,25 +1,9 @@
-import { HiChevronDown, HiChevronUp } from "react-icons/hi";
 import { WidgetVariantGrowthProps } from "./WidgetGrowthVariant.interface";
 import styles from "./WidgetGrowthVariant.module.scss"
 import { Icon } from "../../../Icon";
 import { Button } from "../../../Button";
-import colors from "@/styles/variables/colors.module.scss"
 import { formatter } from "../../../../utils/formatter";
-
-const iconByStatus = {
-  increase: <Icon icon={HiChevronUp} color="success-main" />,
-  decrease: <Icon icon={HiChevronDown} color="error-main" />
-}
-
-const backgroundColorByStatus = {
-  increase: colors["success-lightest"],
-  decrease: colors["error-lightest"]
-}
-
-const colorByStatus = {
-  increase: colors["success-main"],
-  decrease: colors["error-main"]
-}
+import { Pill } from "../../../Pill";
 
 export const WidgetGrowthVariant = ({
   amount,
@@ -28,33 +12,15 @@ export const WidgetGrowthVariant = ({
   percentage,
   button
 }: WidgetVariantGrowthProps) => {
-  const IconComponent = iconByStatus[status]
-
   return (
     <div className={styles.container}>
       <h2 className="semibold">{formatter.toNumber(amount)}</h2>
       <footer>
-        <div className={styles.valuesContainer} style={{
-          backgroundColor: backgroundColorByStatus[status]
-        }}>
-          <div>
-            {IconComponent}
-            <p className="sm medium" style={{
-              color: colorByStatus[status]
-            }}>
-              {formatter.toCompact(difference)}
-            </p>
-          </div>
-
-          <div>
-            {IconComponent}
-            <p className="sm medium"style={{
-              color: colorByStatus[status]
-            }}>
-              {formatter.toPercentage(percentage)}
-            </p>
-          </div>
-        </div>
+        <Pill 
+          status={status}
+          text={formatter.toCompact(difference)} 
+          secondText={formatter.toPercentage(percentage)} 
+        />
 
         {!!button && (
           <Button variant="tertiary" size="extra-small" onClick={button?.onClick}>
